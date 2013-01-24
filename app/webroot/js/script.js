c0bb423ab18c20ca464b6085b146b9a9fd2d821d
gaspard <gaspard@gmail.com>
2013-01-24 15:04:22 +0100

    bootstraping cake-error
    cake-error is alert alert-error

---
+++ app/webroot/js/script.js
@@ -0,0 +1,10 @@
+// 
+//  script.js
+//  app
+//  
+//  Created by gaspard on 2013-01-24.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+// 
+$(document).ready(function(){
+	$('.cake-error').addClass('alert alert-error');
+});
