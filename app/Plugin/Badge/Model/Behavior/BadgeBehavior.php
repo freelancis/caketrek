f01cf2272664585a249c3f0da92b4d7160e46f91
gaspard <gaspard@gmail.com>
2013-01-27 17:06:04 +0100

    creating Plugin/Badge

---
+++ app/Plugin/Badge/Model/Behavior/BadgeBehavior.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* based on Taxonomyehavior
+* @source http://www.grafikart.fr/tutoriels/cakephp/taxonomy-plugin-284 0:13:20
+* 
+* @author gasp
+*/
+class BadgeBehavior extends ModelBehavior{
+	
+	function setup($model,$options= array()){
+		# code...
+	}
+}
+
